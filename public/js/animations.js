8c31724616a812897e72d5e1c993b038ea7ef135
lauirvin <lauirvin98@gmail.com>
2018-03-06 01:11:40 +0000

    Completed card scene

--- public/js/animations.js
+++ public/js/animations.js
@@ -1,3 +1,15 @@
-function scrolltoID() {
+function getStarted() {
+  document.getElementById("3").scrollIntoView({ behavior: "smooth" });
+}
+
+function scrolltoCard1() {
+  document.getElementById("1").scrollIntoView({ behavior: "smooth" });
+}
+
+function scrolltoCard2() {
+  document.getElementById("2").scrollIntoView({ behavior: "smooth" });
+}
+
+function scrolltoCard3() {
   document.getElementById("3").scrollIntoView({ behavior: "smooth" });
 }
