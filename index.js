57ffdd318d34be074df763462a3d313da6bb3356
lauirvin <lauirvin98@gmail.com>
2018-03-19 14:34:50 +0000

    Completed form and database interaction

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 "use strict";
-const readline = require('readline-sync')
-const sqlite3 = require('sqlite3').verbose()
-const dbName = 'hashtags'
 
 const express = require("express");
+const es6Renderer = require("express-es6-template-engine");
 const app = express();
 app.use(express.static("public"));
+
+app.engine("html", es6Renderer);
+app.set("views", "html");
+app.set("view engine", "html");
+
+const publicDir = express.static("public");
+app.use(publicDir);
+
+const bodyParser = require("body-parser");
+const parser = bodyParser.urlencoded({ extended: true });
+app.use(parser);
+
+const sqlite3 = require("sqlite3").verbose();
+
 const port = 8080;
+const DBName = "hashtags";
+
+const db = new sqlite3.Database(`./${DBName}.db`, err => {
+  if (err) return console.error(err.message);
+  console.log(`Connected to the "${DBName}" SQlite database.`);
+});
 
 app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+  const mainCategories = req.query.mainCategories;
+  const subCategories = req.query.subCategories;
+  const data = {
+    title: "TheGramTag"
+  };
+  const sqlSelect = `SELECT ${subCategories} FROM ${mainCategories} ORDER BY RANDOM() LIMIT 5`;
+  console.log(sqlSelect);
+  db.all(sqlSelect, (err, all) => {
+    console.log(all);
+    res.render("getstarted", { locals: data });
+  });
 });
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
-
-
-const db = new sqlite3.Database('./hashtags.db', (err) => {
-	if (err) return console.error(err.message)
-	console.log('Connected to the SQlite database.')
-    db.all('SELECT general FROM photography ORDER BY RANDOM() LIMIT 5', function(err, all) {
-      console.log(all)
-    });
-    db.close((err) => {
-        if (err) return console.error(err.message)
-        console.log('Close the database connection.')
-    })
-})
