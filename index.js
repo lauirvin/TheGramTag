a5d2ca1b37e04eae348c5ff1cfb235386b520595
Irvin Ives Lau <lauirvin98@gmail.com>
2018-03-21 03:35:49 +0000

    Completed working version of value posting to generated page

--- index.js
+++ index.js
@@ -1,49 +1,58 @@
 "use strict";
 
+// Express module configurations
 const express = require("express");
 const es6Renderer = require("express-es6-template-engine");
 const app = express();
+const port = 8080;
 app.use(express.static("public"));
 
 app.engine("html", es6Renderer);
 app.set("views", "html");
 app.set("view engine", "html");
 
-const publicDir = express.static("public");
-app.use(publicDir);
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
 
+// Body-parser module configurations
 const bodyParser = require("body-parser");
-const parser = bodyParser.urlencoded({ extended: true });
-app.use(parser);
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
+app.use(urlencodedParser);
+
+// Sqlite3 module configurations
 
 const sqlite3 = require("sqlite3").verbose();
 
-const port = 8080;
 const DBName = "hashtags";
+
+// SQL Database connection
 
 const db = new sqlite3.Database(`./${DBName}.db`, err => {
   if (err) return console.error(err.message);
   console.log(`Connected to the "${DBName}" SQlite database.`);
 });
 
+// Page configurations
 app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/html/index.html`);
+  res.render('index')
 });
 
 app.get("/getstarted", (req, res) => {
-  const mainCategories = req.query.mainCategories;
-  const subCategories = req.query.subCategories;
   const title = {
     title: "TheGramTag"
   };
+  res.render("getstarted")
+});
+
+app.post("/generated", urlencodedParser, (req, res) => {
+  const mainCategories = req.body.mainCategories;
+  const subCategories = req.body.subCategories;
   const sqlSelect = `SELECT ${subCategories} FROM ${mainCategories} ORDER BY RANDOM() LIMIT 5`;
   console.log(sqlSelect);
   db.all(sqlSelect, (err, all) => {
     console.log(all);
-    res.render("getstarted", { locals: title });
   });
+  res.render("generated")
 });
 
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
-});
