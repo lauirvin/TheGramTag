f4ef6d173b37fa9f731aa64842883b1b27ad0208
lauirvin <lauirvin98@gmail.com>
2018-04-09 01:30:25 +0100

    Refactored styling and added button functions

--- public/js/control.js
+++ public/js/control.js
@@ -13,3 +13,14 @@ function scrolltoCard2() {
 function scrolltoCard3() {
   document.getElementById("3").scrollIntoView({ behavior: "smooth" });
 }
+
+function regenerate() {
+  window.location.reload();
+}
+
+function copytoClipboard() {
+  var copyText = document.getElementById("generated-text");
+  copyText.select();
+  document.execCommand("Copy");
+  alert("Copied to clipboard");
+}
