7abe8bfdb09a6ff01739d266ecd6dceaf9ad318b
lauirvin <lauirvin98@gmail.com>
2018-04-11 16:22:53 +0100

    Refactored #2

--- public/js/control.js
+++ public/js/control.js
@@ -2,16 +2,14 @@ function getStarted() {
   document.getElementById("3").scrollIntoView({ behavior: "smooth" });
 }
 
-function scrolltoCard1() {
-  document.getElementById("1").scrollIntoView({ behavior: "smooth" });
+function scrolltoLeft() {
+  var width = window.innerWidth > 0 ? window.innerWidth : screen.width;
+  document.getElementById("carousel-container").scrollTo(-width - width, 0);
 }
 
-function scrolltoCard2() {
-  document.getElementById("2").scrollIntoView({ behavior: "smooth" });
-}
-
-function scrolltoCard3() {
-  document.getElementById("3").scrollIntoView({ behavior: "smooth" });
+function scrolltoRight() {
+  var width = window.innerWidth > 0 ? window.innerWidth : screen.width;
+  document.getElementById("carousel-container").scrollTo(width + width, 0);
 }
 
 function regenerate() {
@@ -20,7 +18,7 @@ function regenerate() {
 
 function copytoClipboard() {
   var copyText = document.getElementById("generated-text");
-  copyText.select();
+  copyText.focus();
+  copyText.setSelectionRange(0, 2200);
   document.execCommand("Copy");
-  alert("Copied to clipboard");
 }
